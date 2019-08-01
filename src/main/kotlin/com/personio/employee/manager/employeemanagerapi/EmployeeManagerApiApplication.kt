b34547ed5d0d7bf24664c04fad30c5e41fc1c250
Ismaley Viana <ismaley.viana@gmail.com>
2019-07-31 22:52:06 -0300

    initial commit

---
+++ src/main/kotlin/com/personio/employee/manager/employeemanagerapi/EmployeeManagerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.personio.employee.manager.employeemanagerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeManagerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmployeeManagerApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/personio/employee/manager/employeemanagerapi/EmployeeManagerApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.personio.employee.manager.employeemanagerapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EmployeeManagerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
