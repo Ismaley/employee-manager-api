6d0ec831409b2d84fc894eb6b1918bd524373bb3
Ismaley Viana <ismaley.viana@gmail.com>
2019-08-03 15:19:52 -0300

    added find employee by name controller tests

--- src/test/kotlin/com/personio/employee/manager/controller/EmployeeControllerTest.kt
+++ src/test/kotlin/com/personio/employee/manager/controller/EmployeeControllerTest.kt
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -67,15 +68,98 @@ class EmployeeControllerTest {
             "Mike" to "Alan"
         ))
 
-
         mockMvc.perform(post(employeesResource)
             .accept(MediaType.APPLICATION_JSON)
-            .content(employees)
-            .contentType(MediaType.APPLICATION_JSON))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(employees))
             .andExpect(status().isBadRequest)
             .andExpect(jsonPath("$.reason").value(ErrorMessages.INVALID_HIERARCHY_MULTIPLE_BOSSES.split(":")[0]))
             .andExpect(jsonPath("$.message").value(ErrorMessages.INVALID_HIERARCHY_MULTIPLE_BOSSES.split(":")[1]))
             .andDo(MockMvcResultHandlers.print())
     }
 
+    @Test
+    fun `should successfully find an employee by name along with it's hierarchy`() {
+        val employees = mapper.writeValueAsString(mapOf(
+            "Pete" to "Nick",
+            "Barbara" to "Nick",
+            "Nick" to "Sophie",
+            "Sophie" to "Jonas"
+        ))
+
+        val expectedHierarchy = mapOf<String, Map<String, Any>>(
+                "Sophie" to mapOf<String, Map<String, Any>>(
+                    "Nick" to mapOf<String, Map<String, Any>>(
+                        "Pete" to emptyMap(),
+                        "Barbara" to emptyMap()
+                    )
+                )
+        )
+
+        mockMvc.perform(post(employeesResource)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(employees))
+            .andExpect(status().isCreated)
+
+        mockMvc.perform(get(employeesResource)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .param("employeeName", "Sophie"))
+            .andExpect(status().isOk)
+            .andExpect(content().string(mapper.writeValueAsString(expectedHierarchy)))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not find an employee by name if name is empty`() {
+        val employees = mapper.writeValueAsString(mapOf(
+            "Pete" to "Nick",
+            "Barbara" to "Nick",
+            "Nick" to "Sophie",
+            "Sophie" to "Jonas"
+        ))
+
+
+        mockMvc.perform(post(employeesResource)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(employees))
+            .andExpect(status().isCreated)
+
+        mockMvc.perform(get(employeesResource)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .param("employeeName", ""))
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.reason").value(ErrorMessages.EMPTY_NAME.split(":")[0]))
+            .andExpect(jsonPath("$.message").value(ErrorMessages.EMPTY_NAME.split(":")[1]))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not find an employee by name if employee does not exist`() {
+        val notFoundEmployeeName = "Mike"
+        val employees = mapper.writeValueAsString(mapOf(
+            "Pete" to "Nick",
+            "Barbara" to "Nick",
+            "Nick" to "Sophie",
+            "Sophie" to "Jonas"
+        ))
+
+        mockMvc.perform(post(employeesResource)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(employees))
+            .andExpect(status().isCreated)
+
+        mockMvc.perform(get(employeesResource)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .param("employeeName", notFoundEmployeeName))
+            .andExpect(status().isNotFound)
+            .andExpect(jsonPath("$.reason").value(ErrorMessages.NOT_FOUND))
+            .andExpect(jsonPath("$.message").value(ErrorMessages.notFoundError(notFoundEmployeeName).split(":")[1]))
+            .andDo(MockMvcResultHandlers.print())
+    }
 }
